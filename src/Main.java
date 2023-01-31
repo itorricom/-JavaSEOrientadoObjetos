03d54f6140cced90a971cee57f8129818803f3a5
Isidoro Torrico M <itorrico.m@gmail.com>
2023-01-30 21:57:37 -0400

    Metodo constructor

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor();
+        //Doctor myDoctor = new Doctor();
+        Doctor myDoctor = new Doctor("Jairo Siles");
         myDoctor.name = "Isidoro Torrico";
         myDoctor.showName();
     }
