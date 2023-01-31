cf894148bbb9b463c1e36fca6f1d246ec3451eb5
Isidoro Torrico M <itorrico.m@gmail.com>
2023-01-30 23:27:06 -0400

    Creando elementos estaticos

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,11 +1,13 @@
 public class Doctor {
-    int id;
+    //Atributos
+   static int id;  //
     String name;
     String speciality;
 
     //Constructor
     Doctor(){
         System.out.println("Construyendo el objeto Doctor");
+        id++;
     }
     Doctor(String name){
         System.out.println("El nombre del doctor asignado es: " + name);
@@ -14,5 +16,9 @@ public class Doctor {
     //Comportamientos
     public void showName(){
         System.out.println(name);
+    }
+
+    public void  showId(){
+        System.out.println("ID Doctor: " + id);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
+import static ui.UIMenu.*;
+
 public class Main {
     public static void main(String[] args) {
         //Doctor myDoctor = new Doctor();
-        Doctor myDoctor = new Doctor("Jairo Siles");
+        Doctor myDoctor = new Doctor();
         myDoctor.name = "Isidoro Torrico";
         myDoctor.showName();
+        myDoctor.showId();
+
+        Doctor myDoctor1 = new Doctor();
+        myDoctor1.showId();
+
+        showMenu();
+
     }
 }
