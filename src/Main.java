e7d700740e1cff413d5d2348549bf3918dd60a8c
Isidoro Torrico M <itorrico.m@gmail.com>
2023-01-30 23:43:03 -0400

    Final variables Constantes

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,6 @@ import static ui.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
-        //Doctor myDoctor = new Doctor();
-        Doctor myDoctor = new Doctor();
-        myDoctor.name = "Isidoro Torrico";
-        myDoctor.showName();
-        myDoctor.showId();
-
-        Doctor myDoctor1 = new Doctor();
-        myDoctor1.showId();
-
         showMenu();
-
     }
 }
--- src/ui/UIMenu.java
+++ src/ui/UIMenu.java
@@ -3,6 +3,7 @@ package ui;
 import java.util.Scanner;
 
 public class UIMenu {
+    public static String[] MONTHS = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "Nov", "December"};
     public static void showMenu() {
         System.out.println("Welcome to My Appointments");
         System.out.println("Selecciona la opción deseada");
@@ -49,6 +50,9 @@ public class UIMenu {
             switch (response) {
                 case 1:
                     System.out.println("::Book an appointment");
+                    for (int i = 1; i < 4; i++) {
+                        System.out.println(i+" . "+MONTHS[i]);
+                    }
                     break;
                 case 2:
                     System.out.println("::My appointments");
