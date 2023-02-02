e8636edbd57c1a268ef9bdd4845fa6962341e462
Isidoro Torrico M <itorrico.m@gmail.com>
2023-02-01 22:59:59 -0400

    Reto de crear una clase llamado Patient

---
+++ src/Patient.java
@@ -0,0 +1,16 @@
+public class Patient {
+    private String name;
+    private String email;
+    private String address;
+    private String phoneNumber;
+    private String birthday;
+    private  double weight;
+    private souble height;
+    private String blood;
+
+    public Patient(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+}
